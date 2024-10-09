4eca7837ffa6096bc9a978fe3d136f15548d2890
chicooooooooo <gomes.silva.francisco@escola.pr.gov.br>
2024-10-09 11:08:10 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Ciências',
+    'Quantos ossos tem o corpo humano?',
+    '206'
+)
+
+criaCartao(
+    'Filosofia',
+    'De quem é a famosa frase “Penso, logo existo”?',
+    'Descartes'
+)
+
+criaCartao(
+    'Historia',
+    'De onde é a invenção do chuveiro elétrico?',
+    'Brasil'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz tchau em Inglês?',
+    'Tchau em inglês é "Bye" (bai)'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Quais os países que têm a maior e a menor expectativa de vida do mundo?',
+    'Japão e Serra Leoa.'
+)
+
+criaCartao(
+    'Historia',
+    'Quais as duas datas que são comemoradas em novembro?',
+    'Proclamação da República e Dia Nacional da Consciência Negra.)'
+)
