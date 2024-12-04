d617d249100a00b06ef90f44ae821eb08f3145b9
chicooooooooo <gomes.silva.francisco@escola.pr.gov.br>
2024-12-04 10:46:41 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -1,35 +1,179 @@
 criaCartao(
-    'Ciências',
-    'Quantos ossos tem o corpo humano?',
-    '206'
-)
-
-criaCartao(
-    'Filosofia',
-    'De quem é a famosa frase “Penso, logo existo”?',
-    'Descartes'
-)
-
-criaCartao(
-    'Historia',
-    'De onde é a invenção do chuveiro elétrico?',
-    'Brasil'
-)
-
-criaCartao(
-    'Lingua inglesa',
-    'Como se diz tchau em Inglês?',
-    'Tchau em inglês é "Bye" (bai)'
+    'Conhecimentos gerais',
+    'Qual é o maior planeta do Sistema Solar',
+    'Júpiter'
 )
 
 criaCartao(
     'Conhecimentos gerais',
-    'Quais os países que têm a maior e a menor expectativa de vida do mundo?',
-    'Japão e Serra Leoa.'
+    'Qual é o número atômico do oxigênio?',
+    '8'
 )
 
 criaCartao(
-    'Historia',
-    'Quais as duas datas que são comemoradas em novembro?',
-    'Proclamação da República e Dia Nacional da Consciência Negra.)'
+    'Conhecimentos gerais',
+    'Qual é a capital da França?',
+    'Paris'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Quantos continentes existem no mundo?',
+    '7'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Em que ano começou a Primeira Guerra Mundial?',
+    '1914'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Qual o valor de π (pi) arredondado para duas casas decimais?',
+    '3,14'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Quantos graus tem um ângulo reto?',
+    '90 graus'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Qual é o idioma mais falado do mundo?',
+    'Inglês'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Quem escreveu "Dom Quixote?',
+    'Miguel de Cervantes'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Quantos segundos há em um minuto?',
+    '60'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Em que país está localizado o Monte Everest',
+    'Nepal e China'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Qual o maior órgão do corpo humano?',
+    'pele'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Quem pintou a Mona Lisa',
+    'Leonardo da Vinci'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Qual é a moeda oficial do japão?',
+    'Iene (JPY)'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Quantas cores há no arco-íris?',
+    '7'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Quantos dias tem um ano bissexto?',
+    '366'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Quem foi o primriro anoi a pisar na lua?',
+    'Neil Armstrong'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Qual o metal mais abundante na crosta terrestre?',
+    'Alumínio'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Qual é a fórmula química da água?',
+    'H₂O'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Qual é o menor número primo?',
+    '2'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Qual animal é conhecido como rei da selva?',
+    'Leão'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Em que continente está localizado o egito?',
+    'África'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Quantos jogadores formam um time de futebol em campo?',
+    '11 jogadores (10 jogadores de liha e 1 goleiro)'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Qual  é a temperatura de congelamento da água em graus Celsius?',
+    '0 graus Celsius'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Qual é a língua oficial do Brasil?',
+    'Português'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Em que século ocorreu a Revolução Francesa?',
+    'Século XVIII'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Qual é a capital da Austrália?',
+    'Canberra'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Quem inventou a lâmpada elétrica?',
+    'Thomas Edison'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Em que unidade é medida a corrnte elétrica?',
+    'Ampère (A)'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Quantos lados tem um hexágono?',
+    '6'
 )
